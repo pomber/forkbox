2eaa72954adbe55a3841226b6fcce9d5b26ac5fa
pomber <pombopombopombo@gmail.com>
2018-06-23 13:28:29 -0300

    Detect language

--- src/code-panel.js
+++ src/code-panel.js
@@ -2,8 +2,7 @@ import React from "react";
 import MonacoEditor from "react-monaco-editor";
 import { Loader } from "./utils/hitchcock";
 import * as S from "./styles";
-
-const getLanguage = () => "javascript";
+import { getLanguage } from "./utils/gh-entry";
 
 const options = {
   minimap: { enabled: false },
@@ -22,7 +21,7 @@ const CodePanel = ({ path }) => (
           value={text}
           options={options}
           onChange={() => null}
-          language={getLanguage()}
+          language={getLanguage(path)}
         />
       )}
     </Loader>
