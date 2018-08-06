8bc9ee65b7d366e51c29231016bdeda16f612710
pomber <pombopombopombo@gmail.com>
2018-08-06 20:45:29 -0300

    Add home draft

---
+++ src/home.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <div>
+      With ForkBox you can fork, edit, test and make PRs to GitHub repos.
+    </div>
+    <div>
+      Currently, only public repos with a <u>.forkbox</u> folder are supported.
+    </div>
+    <div>ForkBox will need</div>
+    <div>
+      Try it by forking one of the sample repos:
+      <ul>
+        <li>
+          <a>Fork react-storybook-sample</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      Recent forkboxes:
+      <ul>
+        <li />
+      </ul>
+    </div>
+  </div>
+);
+
+export default Home;
