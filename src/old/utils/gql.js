7b93f9aa4b73d837768b8887e21f8895f66202aa
pomber <pombopombopombo@gmail.com>
2018-08-07 12:00:42 -0300

    Remove old code

--- src/old/utils/gql.js
+++
@@ -1,8 +0,0 @@
-export default (strings, token, variables, mapper) =>
-  fetch("https://api.github.com/graphql", {
-    method: "post",
-    body: JSON.stringify({ query: strings.join(""), variables }),
-    headers: new Headers({ authorization: "Bearer " + token })
-  })
-    .then(r => r.json())
-    .then(({ data }) => mapper(data));
